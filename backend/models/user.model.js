dfdc7f385eac457302ef4e162fcf2f62fab0f9bc
Ravi Ranjan <raviranjanxiia20@gmail.com>
2024-04-14 21:59:46 +0530

    Implemented Function
    Made Authcontext provider, improved routes added logout functionality

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -18,25 +18,6 @@ const userSchema = new mongoose.Schema(
     avatarUrl: {
       type: String,
     },
-    // likedProfiles: {
-    // 	type: [String],
-    // 	default: [],
-    // },
-    // likedBy: [
-    // 	{
-    // 		username: {
-    // 			type: String,
-    // 			required: true,
-    // 		},
-    // 		avatarUrl: {
-    // 			type: String,
-    // 		},
-    // 		likedDate: {
-    // 			type: Date,
-    // 			default: Date.now,
-    // 		},
-    // 	},
-    // ],
   },
   { timestamps: true }
 );
